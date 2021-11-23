7d35f76100a2cb383ac4ec4d7393daeb2dbf52dd
EllaGarcia2291 <93016673+EllaGarcia2291@users.noreply.github.com>
2021-11-23 16:37:14 -0600

    Proyecto Ella
    Proyecto Ella

---
+++ Ella Garcia/Scripts/Nivel1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nivel1 : MonoBehaviour
+{
+
+        public void CambiarNivel()
+    {
+        Application.LoadLevel ("Nivel3");
+    }
+}
